569c08b8a479167edf1c8a515a40109b0287ccd5
Vipula S Anandapiya <v.s.anandapiya@gmail.com>
2022-04-08 11:52:17 +0530

    Update 2022_04_08_053851_create_spareparts_table.php

--- database/migrations/2022_04_08_053851_create_spareparts_table.php
+++ database/migrations/2022_04_08_053851_create_spareparts_table.php
@@ -15,7 +15,26 @@ return new class extends Migration
     {
         Schema::create('spareparts', function (Blueprint $table) {
             $table->id();
+
+            $table->bigInteger('product_id')->unsigned()->nullable(); // bigint 10 - unsigned
+            $table->foreign('product_id')->references('id')->on('products');
+
+            $table->bigInteger('manufacturer_id')->unsigned()->nullable(); // bigint 10 - unsigned
+            $table->foreign('manufacturer_id')->references('id')->on('manufacturers');
+
+            $table->bigInteger('vehicle_model_id')->unsigned()->nullable(); // bigint 10 - unsigned
+            $table->foreign('vehicle_model_id')->references('id')->on('vehicle_models');
+
+            $table->string('name'); // varchar 255
+
+            $table->longText('description')->nullable(); // text
+
+            $table->string('image')->nullable(); // varchar 255
+
+            $table->boolean('is_active')->default(true); // tinyint 2 - 0 or 1
+
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
