6354d9fcffb3e4792a23becd8b8169ee330d916e
Vipula S Anandapiya <v.s.anandapiya@gmail.com>
2022-04-22 09:17:56 +0530

    Laravel UI installed

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 Route::get('/', function () {
     return view('welcome');
 });
